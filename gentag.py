1dd1b9c7f9c24f59b13596147817d130970c00a1
mike mcginty <mach327@gmail.com>
2016-12-16 04:31:13 -0500

    updated for api changes

--- gentag.py
+++ gentag.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
 
 import os
-import re
 import sys
 import time
-import kiss
 import aprs
 import datetime
-import binascii
-import string
-import struct
-import reedsolo
 import logging
 
 def packetize_me(me, you, path, name, task, run, data=None, now=None):
@@ -27,12 +21,9 @@ def packetize_me(me, you, path, name, task, run, data=None, now=None):
         frametype="tag"
     else:
         frametype="start"
-    frame = {
-            'source':me,
-            'destination':you,
-            'path':path,
-            'text':"{{%s %s %s %s %.03f%s"%(frametypes[frametype], name, task, run, now, data )
-            }
+    frametxt = "{{%s %s %s %s %.03f%s"%(frametypes[frametype], name, task, run, now, data )
+    framestr = "%s>%s,%s:%s"%( me, you, path, frametxt )
+    frame = aprs.Frame( framestr )
     return frame
 
 def send_event_start_frames(name, task, run, data=None, number_of_frames=5):
@@ -40,6 +31,7 @@ def send_event_start_frames(name, task, run, data=None, number_of_frames=5):
     now=time.time()
     for i in range(number_of_frames):
         frame = packetize_me( me, you, path, name, task, run, data, now )
+        import pdb; pdb.set_trace()
         k.write( frame )
         print(frame)
 
@@ -48,31 +40,22 @@ you = "GPSLL" #laptop
 path = 'WIDE1-1'
 
 if __name__ == "__main__":
-    if sys.argv[1] == 'direwolf':
-        k = aprs.APRSKISS(host="localhost",tcp_port=8001)
-        k._logger.setLevel(logging.DEBUG)
-        k.start()
+    k = aprs.TCPKISS("localhost",8001)
+    k._logger.setLevel(logging.DEBUG)
+    k.start()
+
+    delay = int( sys.argv[1] )
+    name = sys.argv[2]
+    task = sys.argv[3]
+    run = sys.argv[4]
 
 
-        name = sys.argv[3]
-        task = sys.argv[4]
-        run = sys.argv[5]
-        delay = int( sys.argv[2] )
-        try:
-            sys.argv[6]
-            quiet = True
-        except:
-            quiet = False
+    send_event_start_frames( name, task, run, "START", 6)
+    while 1:
+        frame = packetize_me( me, you, path, name, task, run )
+        print(frame)
+        k.write( frame )
+        print(frame)
+        time.sleep( delay )
 
-
-        if not quiet:
-            send_event_start_frames( name, task, run, "START", 6)
-        while 1:
-            frame = packetize_me( me, you, path, name, task, run )
-            k.write( frame )
-            print(frame)
-            time.sleep( delay )
-
-    else:
-        usage(sys.argv)
     
