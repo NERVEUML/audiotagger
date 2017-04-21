ecfc2eeb663610449f033432284c6757c2fe9869
Michael McGinty <mach327@gmail.com>
2017-04-21 19:41:52 -0400

    document by_tag.py a bit

--- by_tag.py
+++ by_tag.py
@@ -66,8 +66,10 @@ class Packet:
         threelines = self.threelines
         #WARNING: depends on grep output to be stable, and must be updated with gentag.py, or will break!
         #Run tests when you install to be sure it works as expected.
-        #depends on Time: lines always being printed before a packet is decoded (in practice this happens)
+        #depends on Time: lines always being printed before a packet is decoded
         #depends on multimon-ng output to be stable
+        #depends on grep output to be stable
+        #if the output of any of these changes, this is what to fix, plus all the regexes that are defined globally in this by_tag.py file.
         #print(threelines)
         s = None
         src = None
@@ -77,7 +79,7 @@ class Packet:
             m = re.match(fullmatch, line )
             if m == None:
                 raise Exception("insufficient matches")
-            bo = m.group("bo")
+            bo = m.group("bo") #byte offset (grep output)
             mn = True if m.group("mn") == ":" else False
             if m.group("time"):
                 s = m.group("seconds")
@@ -124,13 +126,19 @@ class Run:
         self.runname = runname(self.team, self.task, self.run)
         self.files = {p.filename for p in self.packets }
         #originally written for multiple files
-        #but used for only one at a time now
+
+        #BUT used for only one at a time now
         assert len(self.files) == 1
         self.file = [p for p in self.files][0]
+
         self.videofile = [get_orig_fn_from_aprs_file(p) for p in self.files ][0]
         self.calc()
 
     def calc(self):
+        """
+        Figure start and end offsets into file, and real times, based on first and last packets that were decoded.
+        Also figure duration.
+        """
         self.startoffset = self.packets[0].timeoffset
         self.endoffset = self.packets[-1].timeoffset
         self.fileduration = self.endoffset - self.startoffset
@@ -140,6 +148,7 @@ class Run:
         self.duration = (self.end - self.start).total_seconds()
     
     def tojson(self):
+        """Used by json encoder to serialize this object"""
         me = {
             "runname": self.runname,
             "team":self.team,
@@ -160,6 +169,7 @@ class Run:
             self.duration
             ))
     def ffmpegme(self):
+        """Generate ffmpeg lines for cutting the file"""
         out = "_".join([ 
                     self.runname, 
                     hashlib.md5(self.videofile).hexdigest()[:10]
@@ -168,18 +178,22 @@ class Run:
         duration = self.duration + 60 #offset for context, and some more because not all packets decoded properly on all videos
         videofile = self.videofile
         return "ffmpeg -ss %(startoffset)d -i %(videofile)s -to %(duration)d -c copy %(out)s"% locals()
-            
-    
-    
-    
+
     
 def parselines(filename, lines ):
-    tlines = []
-    packets = []
-    ecount = 0
-    for line in lines:
+    """
+    Given APRS lines (from filename, which is just to tell each
+    Packet where it is from), parse them into packets and return
+    the packet objects.  This expects multimon-ng output from the
+    NERVEUML/multimon-ng fork, which includes decoded packet offset data
+    before the decoded packet is printed.
+    """
+    tlines = []  #temporary set of lines, to be passed to Packet
+    packets = [] #successfully decoded packets that will be returned
+    ecount = 0 #count of number of times tlines wasn't able to be decoded
+    for line in lines: #build up lines until we have enough to be a packet, then try and decode and add to packets array
         #print(len(tlines), line)
-        if line.strip() == "--":
+        if line.strip() == "--": #a line matching '^--$' separates decoded packets, because of how we use grep.
             try:
                 p = Packet( filename, tlines )
                 packets.append(p)
@@ -190,12 +204,22 @@ def parselines(filename, lines ):
         else:
             tlines.append(line)
     if ecount >= 5:
-        #5 is ballpark estimate of max number of three lines that shouldn't meet our expectations
+        #5 is ballpark estimate of max number of groups of three lines that shouldn't meet our expectations
         warnings.warn("The input file may not be matching our expectations, check input")
+
     packets.sort(key=lambda x: x.runname )
-    return packets
+    return packets #return packets sorted by runname
 
 def main( argv ):
+    """
+    by_tag.py   <a filename>
+    A filename is the name of the video file being decoded.
+    optional arguments ending in .json and .ffmpeg will be filled with the decoded packets in json format, 
+        and ffmpeg command lines to automatically cut the file.
+    you can have zero, either, or both of these, and order does not matter.
+
+    stdout will get a sorted summary list of runs printed to screen
+    """
     #TODO real argument handling
     jsonfile = None
     ffmpegfile = None
@@ -211,6 +235,8 @@ def main( argv ):
                 ffmpegfile = x
     #packets = parselines( 'stdin', fileinput.input() ) #will read lines from stdin or from filenames as arguments
     packets = parselines( filename, sys.stdin.readlines() ) #just reads lines from stdin
+
+    # dump summary to screen
     runs = {}
     for k,v in groupby( packets, lambda x: x.runname ):
         plist = list(v)
@@ -219,15 +245,16 @@ def main( argv ):
         task = plist[0].task
         runs[k] = Run( team, task, run, plist )
         print(runs[k])
-    if jsonfile:
+
+    if jsonfile: #dump packets to the json file
         with open(jsonfile,"w") as f:
             f.write( json.dumps( runs ))
-    if ffmpegfile:
+
+    if ffmpegfile: #dump ffmpeg command lines to the ffmpeg file (you can cut files automatically with these command lines)
         with open(ffmpegfile,"w") as f:
             for runname,run in runs.items():
                 f.write("%s\n"%(run.ffmpegme()))
-    
-        
+
 
 
 def test():
@@ -240,20 +267,20 @@ def test():
 31-Time: 0.09,fbuf_cnt: 2048, acc=2048, secacc=0
 --
 3855-Time: 8.52,fbuf_cnt: 2066, acc=11444, secacc=8
-3902:AFSK1200: fm WQYC60-9 to GPSLL-0 via WIDE1-1 UI  pid=F0
-3958-{{T draper 4a 3.2 1479400526.886
+3902:AFSK1200: fm callsign-9 to GPSLL-0 via WIDE1-1 UI  pid=F0
+3958-{{T team2 4a 3.2 1479400526.886
 --
 11403-Time: 23.41,fbuf_cnt: 2066, acc=9098, secacc=23
-11451:AFSK1200: fm WQYC60-9 to GPSLL-0 via WIDE1-1 UI  pid=F0
-11507-{{T draper 4a 3.2 1479400541.902
+11451:AFSK1200: fm callsign-9 to GPSLL-0 via WIDE1-1 UI  pid=F0
+11507-{{T team2 4a 3.2 1479400541.902
 --
 148268-Time: 333.35,fbuf_cnt: 2066, acc=7780, secacc=333
-148318:AFSK1200: fm WQYC60-9 to GPSLL-0 via WIDE1-1 UI  pid=F0
-148374-{{S ssci 4a 3.1 1479400847.868 START
+148318:AFSK1200: fm callsign-9 to GPSLL-0 via WIDE1-1 UI  pid=F0
+148374-{{S team1 4a 3.1 1479400847.868 START
 --
 148513-Time: 333.82,fbuf_cnt: 4114, acc=18074, secacc=333
-148564:AFSK1200: fm WQYC60-9 to GPSLL-0 via WIDE1-1 UI  pid=F0
-148620-{{S ssci 4a 3.1 1479400847.868 START
+148564:AFSK1200: fm callsign-9 to GPSLL-0 via WIDE1-1 UI  pid=F0
+148620-{{S team1 4a 3.1 1479400847.868 START
 --
 """
     parselines( 'test', ts.split("\n") )
