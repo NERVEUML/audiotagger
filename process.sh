93ae946209f31f0fc158661ce042f2ad6b581a6c
Michael McGinty <mach327@gmail.com>
2017-04-23 20:15:21 -0400

    remove pointless log files that I will never read anyway

--- process.sh
+++ process.sh
@@ -10,21 +10,21 @@ getoption "$1" target_dir "./"
 
 for subdir in "$target_dir"/*; do
 	if [[ ! -d "$subdir" ]]; then continue; fi
-	"${IMHERE}"/tagall.sh "$subdir" 2>&1 >> "$subdir".tagall_log &
+	"${IMHERE}"/tagall.sh "$subdir" 
 done
 echo "Tagging videos...will return when complete"
 jobs -l
 wait
 for subdir in "$target_dir"/*; do
 	if [[ ! -d "$subdir" ]]; then continue; fi
-	"${IMHERE}"/organize.sh "$subdir" 2>&1 >> "$subdir".organization_log &
+	"${IMHERE}"/organize.sh "$subdir" 
 done
 echo "Running organization...will return when complete"
 jobs -l
 wait
 for subdir in "$target_dir"/*; do
 	if [[ ! -d "$subdir" ]]; then continue; fi
-	"${IMHERE}"/cutter.sh "$subdir" 2>&1 >> "$subdir".cutter_log &
+	"${IMHERE}"/cutter.sh "$subdir" 
 done
 echo "Running ffmpeg cuts...will return when complete"
 jobs -l
